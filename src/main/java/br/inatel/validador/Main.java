c154f9c666b396939d74c3591c961628be11f1e5
Eduardo <edudiasandrade04@gmail.com>
2025-08-19 16:42:16 -0300

    Atualizações na Main e na PlateValidator e README

--- src/main/java/br/inatel/validador/Main.java
+++ src/main/java/br/inatel/validador/Main.java
@@ -1,26 +1,21 @@
 package br.inatel.validador;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Main {
-    // Regex da placa Mercosul: LLLNLNN (L: letra / N: número)
-    private static final String REGEX_PLACA_MERCOSUL = "^[A-Z]{3}[0-9][A-Z][0-9]{2}$";
-
-    public static boolean validarPlaca(String placa) {
-        return Pattern.matches(REGEX_PLACA_MERCOSUL, placa.toUpperCase());
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Informe a placa: ");
+        PlateValidator validator = new PlateValidator();
+
+        System.out.println("Informe a placa:");
         String placa = sc.nextLine();
 
-        if (validarPlaca(placa)) {
-            System.out.println("Placa válida");
+        if (validator.isValid(placa)) {
+            System.out.println("Placa válida!");
         } else {
-            System.out.println("Placa inválida");
+            System.out.println("Placa inválida!");
         }
+
         sc.close();
     }
 }
--- src/main/java/br/inatel/validador/PlateValidator.java
+++ src/main/java/br/inatel/validador/PlateValidator.java
@@ -1,4 +1,12 @@
 package br.inatel.validador;
 
+import java.util.regex.Pattern;
+
 public class PlateValidator {
+
+    // recebe a placa e retorna se é válida ou não
+    public boolean isValid(String plate) {
+        // Regex para placas Mercosul AAA1A23
+        return plate.matches("[A-Z]{3}[0-9][A-Z][0-9]{2}");
+    }
 }
