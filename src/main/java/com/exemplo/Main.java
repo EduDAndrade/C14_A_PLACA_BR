b79b9f5cdac66726314231eb079180d1e2559587
Eduardo <edudiasandrade04@gmail.com>
2025-08-19 15:59:36 -0300

    Atualização no README e na Main

--- src/main/java/com/exemplo/Main.java
+++ src/main/java/com/exemplo/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Digite a placa: ");
+        System.out.print("Informe a placa: ");
         String placa = sc.nextLine();
 
         if (validarPlaca(placa)) {
